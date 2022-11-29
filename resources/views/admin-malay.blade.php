564e06f2fb420e1197aaa32da29663bdce0650fc
Bagus Nugra <bagusnugrahaxd@gmail.com>
2022-11-29 23:54:45 +0700

    fix malay a little

--- resources/views/admin-malay.blade.php
+++ resources/views/admin-malay.blade.php
@@ -60,7 +60,7 @@
   
         <div class="flex bg-gray-300 h-12 p-2 drop-shadow-2xl justify-center sticky top-24">
           <div class="flex flex-row space-x-20">
-            <a href="{{route('listkegiatanpanitia')}}" class="pb-4">
+            <a href="{{route('viewmalayadmin')}}" class="pb-4">
               <p class="font-regular text-black hover:text-pink-600 text-sm">Melayu</p>
               </a>
           </div>
@@ -90,8 +90,8 @@
             <div class="w-24 h-full bg-indigo-500"></div>
           </div>
           <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
-            <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">Space The Final Frontier</h1>
-            <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p>
+            <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">Article to Learn Melayu Language</h1>
+
           </div>
       </div>
       <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
