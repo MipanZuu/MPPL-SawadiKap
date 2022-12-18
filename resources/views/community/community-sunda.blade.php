4fa6722697a6deee7aef7d3ac47dcc2c8863badf
Venia <vsollery@gmail.com>
2022-12-18 20:03:33 +0700

    fix lttle add topic

--- resources/views/community/community-sunda.blade.php
+++ resources/views/community/community-sunda.blade.php
@@ -129,8 +129,8 @@
 
           
           <!--Table-->
-          <a href="">
-          <button  title="Add Article"
+          <a href="/addtopiccommunity">
+          <button  title="Add Topic"
         class="fixed z-90 bottom-10 right-8 bg-blue-600 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300">&#43;</button>
       </a>
           </div>
